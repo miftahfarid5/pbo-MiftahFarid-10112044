d2fbc2be7897603ec5a5a940fa7d2bcb4eee7151
Miftah Farid <129432680+miftahfarid5@users.noreply.github.com>
2026-02-18 20:58:04 +0700

    Add files via upload

---
+++ form_toko.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>Program Penghitung Besar Angsuran Hutang</title>
+</head>
+    <body>
+        
+        <h2>TOKO PEGADAIAN SYARIAH</h2>
+        <h3>Program Penghitung Besar Angsuran Hutang</h3>
+
+        <form action="proses_hitung.php" method="POST">
+            Besar Pinjaman : 
+            <input type="number" name="bs"><br><br>
+
+            Besar Bunga (%) :
+            <input type="number" name="bp"><br><br>
+
+            Lama Angsuran (bulan) :
+            <input type="number" name="la"><br><br>
+
+            Keterlambatan Bayar (hari) :
+            <input type="number" name="kt"><br><br>
+            <input type="submit" value="Hitung">
+    
+        </form>
+
+    </body>
+
+</html>
